acb356987e7b6124859d298b004f4905065f4e1f
001ZQT744 <001ZQT744@IBM-PF33F5QC>
2022-06-01 13:55:00 +0530

    First commit

---
+++ src/com/example/App.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Learning Git");
+		
+		System.out.println("Cloning the code");
+		
+		System.out.println("Pushing into git");
+	}
+
+}
